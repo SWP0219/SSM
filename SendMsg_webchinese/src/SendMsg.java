e91aa86a9345577117dfa90625427a674c00b1c8
SWP0219 <shangwenpeng_x@163.com>
2017-05-21 12:58:15 +0800

    首次提交到本地仓库

---
+++ SendMsg_webchinese/src/SendMsg.java
@@ -0,0 +1,31 @@
+import org.apache.commons.httpclient.Header;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.NameValuePair;
+import org.apache.commons.httpclient.methods.PostMethod;
+public class SendMsg{
+
+public static void main(String[] args)throws Exception
+{
+
+HttpClient client = new HttpClient();
+PostMethod post = new PostMethod("http://gbk.sms.webchinese.cn"); 
+post.addRequestHeader("Content-Type","application/x-www-form-urlencoded;charset=gbk");//在头文件中设置转码
+NameValuePair[] data ={ new NameValuePair("Uid", "pengge1191"),new NameValuePair("Key", "3bc71f096ade780a9cf8"),new NameValuePair("smsMob","18516590873"),new NameValuePair("smsText","短信内容")};
+post.setRequestBody(data);
+
+client.executeMethod(post);
+Header[] headers = post.getResponseHeaders();
+int statusCode = post.getStatusCode();
+System.out.println("statusCode:"+statusCode);
+for(Header h : headers)
+{
+System.out.println(h.toString());
+}
+String result = new String(post.getResponseBodyAsString().getBytes("gbk")); 
+System.out.println(result); //打印返回消息状态
+
+
+post.releaseConnection();
+
+}
+}
